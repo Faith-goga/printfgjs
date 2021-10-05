c166c0ca5395aa5533082dc45abd34630c17344e
Faith-goga <83953243+Faith-goga@users.noreply.github.com>
2021-10-05 23:00:52 +0300

    This should work please

--- holberton.h
+++ holberton.h
@@ -1,22 +1,38 @@
-#ifndef HEADER_HOLBERTON
-#define HEADER_HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/**
+* struct types_arguments - Struct op
+* @t: The operator
+* @f: The function
+*/
+typedef struct types_arguments
+{
+	char t;
+	int (*f)(va_list vl);
+} funcs;
+
 int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
+int print_char(va_list vl);
+int print_string(va_list vl);
+int print_number(va_list vl);
+void print_num_rec(int n);
+int print_rev(va_list vl);
+int print_rot13(va_list vl);
+int print_binary(va_list vl);
+int print_unsigned(va_list vl);
+int print_octal(va_list vl);
+int print_hex_lower(va_list vl);
+int print_hex_upper(va_list vl);
+int print_hex(unsigned long int c, int cap);
+int print_pointer(va_list vl);
+int print_special_chars(va_list vl);
 int _printf(const char *format, ...);
-char *convert_to(char representation[], unsigned int num, int base);
-int function_manager(char c, va_list arg);
-int _switch(char c, va_list arg);
-int print_character(va_list arg);
-int print_sign(va_list arg, int base);
-int print_unsign(va_list arg, int base);
-int print_string(va_list arg);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_rev(va_list arg);
-int print_base16_upper_lower(va_list arg, char *representation);
-int loop_format(va_list arg, const char *string);
-int call_function_manager(char aux, va_list arg);
-int check_percent(int *flag, char aux);
+
 #endif
